3900338394128942745674e8f422ccf341fc0730
jspc <james@zero-internet.org.uk>
2013-12-06 16:40:05 +0000

    WIP

--- capistrano-ext-chef.gemspec
+++ capistrano-ext-chef.gemspec
@@ -13,9 +13,7 @@ Gem::Specification.new do |spec|
   spec.license = 'Simplified BSD'
 
   spec.add_dependency 'capistrano', '>=2.11.0'
-  spec.add_dependency 'net-ssh', '2.3.0'
   spec.add_dependency 'capistrano-ext'
-  spec.add_dependency 'knife-solo'
 
   spec.add_development_dependency 'rspec'
   spec.add_development_dependency 'capistrano-spec'
--- lib/capistrano/ext/chef.rb
+++ lib/capistrano/ext/chef.rb
@@ -1,6 +1,18 @@
 require 'capistrano'
 module Capistrano
   module Chef
+    def self.set_shell bundle
+      if bundle
+        'bundle exec'
+      end
+    end
+
+    def self.key_string k
+      if k
+        "-i #{k}"
+      end
+    end
+
     def self.load_into conf
       conf.load do
         before 'deploy:setup',       'chef:bootstrap'
@@ -13,8 +25,14 @@ module Capistrano
             chef_dir  = fetch(:chef_dir, '/tmp/chef')
             bootstrap = fetch(:chef_bootstrap, 'bootstrap')
             user      = fetch(:user, ENV['USER'])
-            fetch(:chef_hosts).each do |h|
-              run_locally "cd #{chef_dir} && knife bootstrap -x #{user} -d #{bootstrap} #{h}"
+            bundle    = fetch(:chef_bundle, false)
+            key       = fetch(:chef_key, nil)
+
+            if bundle
+              puts "cd #{chef_dir} && bundle install"
+            end
+            fetch(:chef_hosts, []).each do |h|
+              puts "cd #{chef_dir} && #{Chef.set_shell(bundle)} knife bootstrap -x #{user} #{Chef.key_string(key)} -d #{bootstrap} #{h}"
             end
           end
           
@@ -24,8 +42,11 @@ module Capistrano
             chef_dir  = fetch(:chef_dir, '/tmp/chef')
             env       = fetch(:environment, 'production')
             user      = fetch(:user, ENV['USER'])
-            fetch(:chef_hosts).each do |h|
-              run_locally "cd #{chef_dir} && knife solo cook #{user}@#{h}"
+            bundle    = fetch(:chef_bundle, false)
+            key       = fetch(:chef_key, nil)
+            
+            fetch(:chef_hosts, []).each do |h|
+              puts "cd #{chef_dir} && #{Chef.set_shell(bundle)} knife solo cook #{user}@#{h} #{Chef.key_string(key)}"
             end
           end # chef:provision
         end # chef
